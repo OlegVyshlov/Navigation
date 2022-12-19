d4fdf02aea6dfa7d03a39c2d8f78e3089ad6b552
Oleg Vyshlov <vyshlov@gmail.com>
2022-12-19 14:41:53 +0300

    DZ AutoLayout v.2

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -11,12 +11,11 @@ struct Profile {
     
     var fullName: String
     var status: String
-    var statusText: String
 }
 
 class ProfileHeaderView: UIView {
     
-    lazy var profile = Profile(fullName: "Profile", status: "Skilled Driver", statusText: "Waiting for something...")
+    lazy var profile = Profile(fullName: "Skilled Driver", status: "Waiting for something...")
     
     private let avatarImageView: UIImageView = {
         let avatar = UIImageView()
@@ -32,20 +31,12 @@ class ProfileHeaderView: UIView {
     private let fullNameLabel: UILabel = {
         let label = UILabel()
         label.textColor = .black
-        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.font = UIFont.boldSystemFont(ofSize: 18)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
         
     private let statusLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .black
-        label.font = UIFont.boldSystemFont(ofSize: 18)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let statusTextField: UILabel = {
         let label = UILabel()
         label.textColor = .gray
         label.font = UIFont.systemFont(ofSize: 14)
@@ -66,18 +57,12 @@ class ProfileHeaderView: UIView {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
-    private let newButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("New button", for: .normal)
-        button.setTitleColor(UIColor.black, for: .normal)
-        button.backgroundColor = .systemYellow
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
 
+
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+//        backgroundColor = .cyan
         setupUI()
         setupConstraints()
         addTargets()
@@ -91,19 +76,17 @@ class ProfileHeaderView: UIView {
         addSubview(avatarImageView)
         addSubview(fullNameLabel)
         addSubview(statusLabel)
-        addSubview(statusTextField)
         addSubview(setStatusButton)
-        addSubview(newButton)
+        
         
         fullNameLabel.text = profile.fullName
         statusLabel.text = profile.status
-        statusTextField.text = profile.statusText
     }
     
    
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 116),
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
             avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             avatarImageView.heightAnchor.constraint(equalToConstant: 120),
             avatarImageView.widthAnchor.constraint(equalToConstant: 120),
@@ -113,34 +96,25 @@ class ProfileHeaderView: UIView {
             setStatusButton.heightAnchor.constraint(equalToConstant: 50),
             setStatusButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
             
-            newButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
-            newButton.rightAnchor.constraint(equalTo: rightAnchor, constant: 0),
-            newButton.topAnchor.constraint(equalTo: bottomAnchor, constant: 0),
-            newButton.heightAnchor.constraint(equalToConstant: 50),
+           
             
-            fullNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 60),
-            fullNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            fullNameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor, constant: 16),
+            fullNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 160),
 
-            statusLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor, constant: 16),
-            statusLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 160),
-
-            statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -27),
-            statusTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 160)
+            statusLabel.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -27),
+            statusLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 160)
             ])
-
     }
 
     private func addTargets() {
         setStatusButton.addTarget(self, action: #selector(tapOnButton), for: .touchUpInside)
-        newButton.addTarget(self, action: #selector(newTapOnButton), for: .touchUpInside)
+        
     }
     
     @objc func tapOnButton() {
         print("Blue button")
     }
     
-    @objc func newTapOnButton() {
-        print("New button")
-    }
+
 
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,26 +8,53 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-
+    
     private let profileHeaderView: ProfileHeaderView = {
         let view = ProfileHeaderView()
         return view
+    }()
+
+    private let newButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("New button", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = .systemYellow
+        button.layer.cornerRadius = CGRectGetHeight(button.bounds) / 4
+        button.layer.shadowOffset = CGSize.init(width: 4, height: 4)
+        button.layer.shadowRadius = 4.0
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(ProfileViewController.self, action: #selector(newTapOnButton), for: .touchUpInside)
+        return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
         setupLayout()
+        title = "Profile"
+    }
+    
+    @objc func newTapOnButton() {
+        print("New button")
     }
 
     func setupLayout() {
         view.addSubview(profileHeaderView)
+        view.addSubview(newButton)
         profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            profileHeaderView.topAnchor.constraint(equalTo: view.topAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+            profileHeaderView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200),
+
+            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            newButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            newButton.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
+    
 }
